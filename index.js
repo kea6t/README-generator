7e97205af558a71ae69f16735262fccb62dc2b2e
kea6t <keasay26@gmail.com>
2022-07-14 01:45:46 -0400

    created an array of questions for user input. Also tried to create a function for license passed but have not tested it yet.

--- index.js
+++ index.js
@@ -1,13 +1,119 @@
 // TODO: Include packages needed for this application
-
+const { writeFile } = require('./utils/generateMarkdown.j');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your project? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide a description of the project (Required)',
+                validate: descriptionInput => {
+                    if (descriptionInput) {
+                        return true;
+                    } else {
+                        console.log('You need to enter a project description!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'list',
+                name: 'contents',
+                message: 'Table of contents (Required)',
+                validate: contentsInput => {
+                    if (contentsInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide a list of contents!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'install',
+                message: 'Describe the installation of a project(Required)',
+                validate: installInput => {
+                    if (installInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide a description of the installation!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Describe the usage of a project(Required)',
+                validate: usageInput => {
+                    if (usageInput) {
+                        return true;
+                    } else {
+                        console.log('Please provide a description usage!');
+                        return false;
+                    } 
+                }
+            },
+            {
+                type: 'checkbox',
+                name: 'license',
+                message: 'What license did you use for this project? (Check all that apply)',
+                choices: ['Apache', 'GNU', 'MIT']
+            },
+            {
+                type: 'input',
+                name: 'contributing',
+                message: 'Who contributed to this project?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your GitHub Username (Required)',
+                validate: githubInput => {
+                  if (githubInput) {
+                    return true;
+                  } else {
+                    console.log('Please enter your GitHub username!');
+                    return false;
+                  }
+                }
+              },
+            {
+                type: 'input',
+                name: 'test',
+                message: 'Are there tests performed?'
+            },
+            {
+                type: 'input',
+                name: 'questions',
+                message: 'To be or not to be, that is the question?'
+            }
+        ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,32 @@
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = license => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/README.md', license, err => {
+      // if there's an error, reject the promise and send the error to the Promise's `.catch()` method
+      if (err) {
+        reject(err);
+        // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+        return;
+      }
+      // if everything went well, resolve the promise and send the successful data to the `.then()` method
+      resolve({
+        ok: true,
+        message: 'File Created'
+      });
+    });
+  });
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
