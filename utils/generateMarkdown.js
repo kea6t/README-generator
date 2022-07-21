ed7f37b2675aa0c6ad1e3acdeacee64edbc4748e
kea6t <keasay26@gmail.com>
2022-07-21 01:43:16 -0400

    final touches

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,13 +9,13 @@ const renderLicenseBadge = license => {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 const renderLicenseLink= license =>{
-  !license ?  ``: `* [License](#license)`; 
+  !license ?  ``: `- [License](#license)`; 
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 const renderLicenseSection = license => {
-  !license ?  ``: `## License ${license}`;
+  !license ?  ``: `## License`;
 }
 
 // TODO: Create a function to generate markdown for README
